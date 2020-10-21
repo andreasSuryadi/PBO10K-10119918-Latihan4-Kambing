712ca66b1b75fd466fb4de827bd6471de8c781dc
Andreas Suryadi <andreas.suryadi02@gmail.com>
2020-10-21 07:50:04 +0700

    Update identitas data diri

--- src/main/java/TugasPBO4/Kambing.java
+++ src/main/java/TugasPBO4/Kambing.java
@@ -7,7 +7,13 @@ package TugasPBO4;
 
 /**
  *
- * @author andreas
+ * @author
+ * NAMA     : Andreas Suryadi
+ * KELAS    : PBO10K
+ * NIM      : 10119918
+ * Deskripsi Program : Program ini berisi program untuk mangakses function dari 
+ * class yang sama
+ * 
  */
 public class Kambing {
     
